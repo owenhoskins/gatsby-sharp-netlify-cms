c237b86c9500b953f59215e06c134d13c5e05c48
Owen Hoskins <owen.hoskins@gmail.com>
2018-01-20 17:07:50 +0200

    added basic gallery for testing

--- src/templates/gallery.js
+++ src/templates/gallery.js
@@ -1,23 +1,87 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import PhotoGallery from '../components/Gallery/'
+import Img from 'gatsby-image'
 
-export default ({ data }) => {
-  console.log('gallery.js data: ', data)
-  const { markdownRemark: post } = data;
+const renderImage = (props) => {
+  const {
+    photo: { width, height, originalSizes },
+    margin,
+    onClick,
+  } = props
   return (
-    <div>
-      <h2>{post && post.frontmatter && post.frontmatter.title}</h2>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    <div
+      style={{
+        width,
+        height,
+        float: 'left',
+        margin,
+        cursor: 'pointer'
+      }}
+      onClick={(evt) => onClick(evt, props)}
+    >
+      <Img sizes={originalSizes} />
     </div>
+  )
+}
+
+export default function Template({ data }) {
+  const { markdownRemark: post } = data;
+  const photos = []
+  post.frontmatter.images.forEach(({
+    image
+    //image: { childImageSharp: { sizes }}
+  }) => {
+    if (image.childImageSharp) {
+      const { childImageSharp: { sizes }} = image
+      const { aspectRatio, src, srcSet } = sizes
+      photos.push({
+        width: aspectRatio, height: 1, src,
+        srcSet: srcSet.split(","),
+        sizes: [sizes.sizes],
+        originalSizes: sizes,
+      })
+    } else {
+      console.log("!!does not have childImageSharp: ", image)
+    }
+  })
+
+  return (
+    <section>
+      <Helmet title={`${post.frontmatter.title}`} />
+      <div>
+        <h1>
+          {post.frontmatter.title}
+        </h1>
+        <div>
+          <PhotoGallery
+            renderImage={renderImage}
+            photos={ photos }
+          />
+        </div>
+        <h2>Biography</h2>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </section>
   );
-};
+}
 
 export const pageQuery = graphql`
-  query Page($path: String!) {
+  query GalleryByPath($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
         path
         title
+        images {
+          image {
+            childImageSharp {
+             sizes(maxWidth: 700) {
+                ...GatsbyImageSharpSizes
+             }
+            }
+          }
+        }
       }
     }
   }
