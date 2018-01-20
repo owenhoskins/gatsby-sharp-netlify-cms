089ff0d496f164259972752237eb507fe164148c
Owen Hoskins <owen.hoskins@gmail.com>
2018-01-20 18:33:49 +0200

    test creation of a widget

---
+++ src/cms/GalleryManager.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+// This is the editing component
+export class GalleryControl extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+// This is the preview component
+export const GalleryPreview = props => <div></div>;
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,8 @@
-import CMS from "netlify-cms";
+import CMS from 'netlify-cms'
 
 //import { SlidesControl, SlidesPreview } from "./Slides";
 //CMS.registerWidget("slides", SlidesControl, SlidesPreview);
+
+import { GalleryControl, GalleryPreview } from './GalleryManager'
+
+CMS.registerWidget('gallery', GalleryControl, GalleryPreview);
