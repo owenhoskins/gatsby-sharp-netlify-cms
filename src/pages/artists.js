2b6d59d93713e8bb771d5bfacc42a4fb50f6daa1
Owen Hoskins <owen.hoskins@gmail.com>
2018-03-10 16:20:02 +0200

    testing responsive horizontal scroller based on scroll precentage

--- src/pages/artists.js
+++ src/pages/artists.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Link from 'gatsby-link'
 import { ScrollHorizontal, ScrollTop } from '../components/Scroll'
 import { HeaderXS, HeaderSM, HeaderMD, HeaderLG } from '../components/Styled'
+import { Motion, spring, presets } from 'react-motion'
+const springConfig = presets.noWobble
 
 import Menu from '../components/Menu/Services'
 
@@ -78,99 +80,113 @@ export default class ArtistsPage extends Component {
           scrollToSection={this.scrollToSection}
           currentSection={this.state.inViewIndex}
         />
-
         {
-          dataArray && dataArray.map((type, index) => {
-            const refKey = Object.keys(data)[index]
-            // console.log('refKey: ', refKey, type)
-            return (
-              <ScrollPercentage
-                key={refKey}
-                onChange={(percentage, inView) => this.handleChange({percentage, inView, index, refKey})}
-                innerRef={(ref) => this.returnRef(ref, refKey)}
-              >
-              {
-                //const leftEdge = 0.75
-                //const rightEdge = 0.2
-                (percentage) => {
-                  let opacity
-                  if (percentage > leftEdge) { // edge of menu
-                    opacity = 0.5 - percentage.toPrecision(2)
-                  } else if (percentage < rightEdge) { // right edge
-                    opacity = percentage.toPrecision(2)
-                  } else {
-                    opacity = percentage.toPrecision(2) * 1.5
-                  }
 
-                  return (
-                    <div
-                      css={{
-                        width: `calc(100vw - 21rem)`,
-                        height: `20vh`,
-                        marginBottom: '1vh',
-                        //backgroundColor: 'rgba(0, 0, 0, 0.1)'
-                      }}
-                      //threshold='0.7'
-                    >
-                      <div
-                        css={{
-                          //transition: `opacity 200ms linear, 800ms filter linear`,
-                          display: 'flex',
-                          position: `fixed`,
-                          top: `0`,
-                          bottom: `0`,
-                          left: `calc(100vw)`,
-                          width: `100vw`
-                        }}
-                        style={{
-                          opacity: opacity,
-                          transform: `translateX(${-(percentage * 100) + 'vw'})`,
-                          filter: percentage < rightEdge || percentage > leftEdge ? `blur(1px)` : `blur(0)`
-                        }}
-                      >
-                        <div
-                          css={{
-                            width: `21rem`,
-                            height: `29rem`,
-                            alignSelf: `center`
-                            //backgroundColor: 'rgba(0, 0, 0, 0.1)'
-                          }}
-                        >
-                          {
-                            type.edges.map(({ node: { fields, frontmatter }}) => {
-                              return (
-                                <HeaderLG
-                                  key={fields.slug}
-                                >
-                                  <Link
-                                    to={fields.slug}
-                                    onClick={e => this.handleClick(e, fields.slug, frontmatter.type)}
-                                  >{frontmatter.title}</Link>
-                                </HeaderLG>
-                              )
-                            })
-                          }
-                          <p>{refKey}</p>
-                          <p>{`Percentage scrolled: ${percentage.toPrecision(2)}%.`}</p>
-                        </div>
-                      </div>
-                      <p>{refKey}</p>
-                          <p>{`Percentage scrolled: ${percentage.toPrecision(2)}%.`}</p>
+          // 1% is past the viewport
+          // .5% is dead centered in the viewport
+          // 0% is before the viewport
 
-                    </div>
-                  )
-                }
+          <ScrollPercentage
+            //key={refKey}
+            //onChange={(percentage, inView) => this.handleChange({percentage, inView, index, refKey})}
+            //innerRef={(ref) => this.returnRef(ref, refKey)}
+          >
+          {
+            //const leftEdge = 0.75
+            //const rightEdge = 0.2
+            (percentage) => {
+              let opacity
+              if (percentage > leftEdge) { // edge of menu
+                opacity = 0.5 - percentage.toPrecision(2)
+              } else if (percentage < rightEdge) { // right edge
+                opacity = percentage.toPrecision(2)
+              } else {
+                opacity = percentage.toPrecision(2) * 1.5
               }
-              </ScrollPercentage>
-            )
-          })
+
+              opacity = 1
+
+              return (
+                <div>
+                  {
+                    dataArray && dataArray.map((type, index) => {
+                      const refKey = Object.keys(data)[index]
+                      // console.log('refKey: ', refKey, type)
+                      return (
+                        <Motion style={ { z: spring(-percentage.toPrecision(2) * 100, springConfig) } }>
+                          { ({z}) => {
+                            return (
+
+                            <div
+                              css={{
+                                //width: `calc(100vw - 21rem)`,
+                                height: `49vh`,
+                                marginBottom: '1vh',
+                                //backgroundColor: 'rgba(0, 0, 0, 0.1)'
+                              }}
+                              //threshold='0.7'
+                            >
+                              <div
+                                css={{
+                                  //transition: `opacity 200ms linear, 800ms filter linear`,
+                                  display: 'flex',
+                                  position: `fixed`,
+                                  top: `0`,
+                                  bottom: `0`,
+                                  left: `calc((23rem * ${index + 1}))`,
+                                  //width: `100vw`
+                                  // left was 100vw
+                                  //left: `calc((23rem * ${index + 1}) + 100vw)`
+                                }}
+                                style={{
+                                  opacity: opacity,
+                                  //transform: `translateX(${-(percentage * 100) + 'vw'})`,
+                                  //transform: `translateX(${-(percentage * 23) + 'rem'})`,
+                                  //filter: percentage < rightEdge || percentage > leftEdge ? `blur(1px)` : `blur(0)`
+                                  transform: `translateX(${z}%)`
+                                }}
+                              >
+                                <div
+                                  css={{
+                                    width: `21rem`,
+                                    height: `29rem`,
+                                    alignSelf: `center`
+                                    //backgroundColor: 'rgba(0, 0, 0, 0.1)'
+                                  }}
+                                >
+                                  {
+                                    type.edges.map(({ node: { fields, frontmatter }}) => {
+                                      return (
+                                        <HeaderLG
+                                          key={fields.slug}
+                                        >
+                                          <Link
+                                            to={fields.slug}
+                                            onClick={e => this.handleClick(e, fields.slug, frontmatter.type)}
+                                          >{frontmatter.title}</Link>
+                                        </HeaderLG>
+                                      )
+                                    })
+                                  }
+                                  <p>{refKey}</p>
+                                  <p>{`Percentage scrolled: ${percentage.toPrecision(2)}%.`}</p>
+                                </div>
+                              </div>
+                                {/*<p>{refKey}</p>
+                                  <p>{`Percentage scrolled: ${percentage.toPrecision(2)}%.`}</p>*/}
+                            </div>
+                          )
+                        }}
+                        </Motion>
+                      )
+                    })
+                  }
+                </div>
+              )
+            }
+          }
+        </ScrollPercentage>
         }
-        <div
-          css={{
-            width: `calc(100vw - 21rem)`,
-            height: `21rem`,
-          }}
-        />
       </div>
     )
   }
